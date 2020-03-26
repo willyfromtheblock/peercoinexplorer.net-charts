fb745eabc2f4c2c714131e6bd7b388bf534bc879
bananenwilly <pussinbootsdev@gmail.com>
2020-03-26 11:38:03 +0100

    refactor footer, change footer to fixed

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SentryBoundary from "./components/sentry";
 import * as Sentry from "@sentry/browser";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import Footer from "./components/footer";
 
 class App extends Component {
   state = {
@@ -78,9 +79,7 @@ class App extends Component {
     window.history.pushState(
       null,
       "",
-      `/charts/${this.state.selectedChart}/${rangeSelected}/${
-        this.state.option
-      }/${this.state.selectedGroup}`
+      `/charts/${this.state.selectedChart}/${rangeSelected}/${this.state.option}/${this.state.selectedGroup}`
     );
   };
 
@@ -257,7 +256,6 @@ class App extends Component {
       loading,
       rangeSelected,
       seriesOptions,
-      extremes,
       selectedGroup
     } = this.state;
 
@@ -383,98 +381,88 @@ class App extends Component {
       <React.Fragment>
         <SentryBoundary>
           <ToastContainer />
-          <div
-            style={{
-              position: "absolute",
-              width: "100%",
-              minWidht: "100vw",
-              minHeight: "100vh"
-            }}
-          >
-            {loading && (
-              <div className="loader">
-                <Loader
-                  type="RevolvingDot"
-                  color="#3cb054"
-                  height="100"
-                  width="100"
-                />
-              </div>
-            )}
-            <header>
-              <div className="navbar_ppc navbar-dark shadow-sm">
-                <div className="container d-flex justify-content-between">
-                  <img
-                    className="logo"
-                    style={{ maxWidth: "100vw", margin: 10 }}
-                    src="https://peercoinexplorer.net/peercoin-horizontal-greenleaf-whitetext-transparent.svg"
-                    alt="Peercoin Logo"
+          <div>
+            <div
+              style={{
+                position: "absolute",
+                width: "100%",
+                minHeight: "100vh"
+              }}
+            >
+              {loading && (
+                <div className="loader">
+                  <Loader
+                    type="RevolvingDot"
+                    color="#3cb054"
+                    height="100"
+                    width="100"
                   />
                 </div>
-              </div>
-            </header>
-            <main role="main">
-              <section className="jumbotron text-center">
-                <div className="container">
-                  <DonationModal
-                    modalShow={modalShow}
-                    hideModal={this.hideModal}
-                  />
-                  <h1 className="jumbotron-heading">Peercoin charts</h1>
-                  <hr />
-                  <ButtonGroup
-                    selected={selectedChart}
-                    charts={charts}
-                    raiseLoad={this.handleLoad}
-                  />
-                  <div id="chartContainer" />
-                  {!initialLoad ? (
-                    <div className="jumbotron jumbotron-fluid">
-                      <div className="container">
-                        <h5>Please select a chart to load.</h5>
+              )}
+              <header>
+                <div className="navbar_ppc navbar-dark shadow-sm">
+                  <div className="container d-flex justify-content-between">
+                    <img
+                      className="logo"
+                      style={{ maxWidth: "100vw", margin: 10 }}
+                      src="https://peercoinexplorer.net/peercoin-horizontal-greenleaf-whitetext-transparent.svg"
+                      alt="Peercoin Logo"
+                    />
+                  </div>
+                </div>
+              </header>
+              <main role="main">
+                <section className="jumbotron text-center">
+                  <div className="container">
+                    <DonationModal
+                      modalShow={modalShow}
+                      hideModal={this.hideModal}
+                    />
+                    <h1 className="jumbotron-heading">Peercoin charts</h1>
+                    <hr />
+                    <ButtonGroup
+                      selected={selectedChart}
+                      charts={charts}
+                      raiseLoad={this.handleLoad}
+                    />
+                    <div id="chartContainer" />
+                    {!initialLoad ? (
+                      <div className="jumbotron jumbotron-fluid">
+                        <div className="container">
+                          <h5>Please select a chart to load.</h5>
+                        </div>
                       </div>
+                    ) : (
+                      <div ref={"optionsGroup"}>
+                        <OptionsGroup
+                          options={options}
+                          dataGroups={dataGroups}
+                          selectedOption={option}
+                          selectedGroup={selectedGroup}
+                          raiseOptions={this.handleOptions}
+                          raiseGroup={this.handleGroup}
+                        />
+                      </div>
+                    )}
+                    <p style={{ marginTop: "10px", marginBottom: "10px" }}>
+                      <small>Data is updated every 24 hours.</small>
+                    </p>
+                    <div style={{ marginBottom: "2rem" }}>
+                      <GitHubButton
+                        href="https://github.com/bananenwilly/peercoinexplorer.net-charts/issues"
+                        data-icon="octicon-issue-opened"
+                        data-size="large"
+                        data-show-count="true"
+                        aria-label="Issue bananenwilly/peercoinexplorer.net-charts on GitHub"
+                      >
+                        Issue
+                      </GitHubButton>
                     </div>
-                  ) : (
-                    <div ref={"optionsGroup"}>
-                      <OptionsGroup
-                        options={options}
-                        dataGroups={dataGroups}
-                        selectedOption={option}
-                        selectedGroup={selectedGroup}
-                        raiseOptions={this.handleOptions}
-                        raiseGroup={this.handleGroup}
-                      />
-                    </div>
-                  )}
-                  <p style={{ marginTop: "10px", marginBottom: "10px" }}>
-                    <small>Data is updated every 24 hours.</small>
-                  </p>
-                  <GitHubButton
-                    href="https://github.com/bananenwilly/peercoinexplorer.net-charts/issues"
-                    data-icon="octicon-issue-opened"
-                    data-size="large"
-                    data-show-count="true"
-                    aria-label="Issue bananenwilly/peercoinexplorer.net-charts on GitHub"
-                  >
-                    Issue
-                  </GitHubButton>
-                </div>
-              </section>
-            </main>
-            <footer className="footer navbar_ppc">
-              <div className="container">
-                <p className="donate_addr text-light">
-                  If you're enjoying this service, please consider donating to
-                  <button
-                    type="button"
-                    onClick={() => this.showModal()}
-                    className="btn btn-secondary donate_addr"
-                  >
-                    PPXMXETHJE3E8k6s8vmpDC18b7y5eKAudS
-                  </button>
-                </p>
-              </div>
-            </footer>
+                  </div>
+                </section>
+              </main>
+            </div>
+            <Footer raiseShowModal={this.showModal} />
           </div>
         </SentryBoundary>
       </React.Fragment>
